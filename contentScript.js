e6bc0fbdb45719702065f20a497d779498324a04
George Ridgway <georger361@gmail.com>
2017-07-13 16:57:22 -0500

    8Values now appears when clicked
    added seperate file for JS since chrome banns use of inline JS for security

---
+++ contentScript.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+      document.getElementById("8valuesCLick").addEventListener("click", function(){
+        document.getElementById("8iframe").src='https://8values.github.io/quiz.html';
+        document.getElementById("8iframe").height="100%";
+        document.getElementById("8iframe").width="100%";
+      });
+});
