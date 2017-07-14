b44ea62c523316c6b826a5d2dc5e89ba4376c88a
George Ridgway <georger361@gmail.com>
2017-07-14 14:23:17 -0500

    spekr now works, need to work on the looks

--- contentScript.js
+++ contentScript.js
@@ -3,5 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("8iframe").src='https://8values.github.io/quiz.html';
         document.getElementById("8iframe").height="100%";
         document.getElementById("8iframe").width="100%";
+        document.getElementById("8iframe").target="_parent";
+      });
+      document.getElementById("spekrValuesClick").addEventListener("click", function(){
+        document.getElementById("spekrIframe").src='http://spekr.org/';
+        document.getElementById("spekrIframe").height="100%";
+        document.getElementById("spekrIframe").width="100%";
+        document.getElementById("spekrIframe").target="_parent";
       });
 });
