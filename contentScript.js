97ddba776c37a94a26f608835d04e8133a9a1183
George Ridgway <georger361@gmail.com>
2017-07-16 12:08:24 -0500

    popup will now resize
    need to work on transforming the content inside the iframe

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
       document.getElementById("8valuesCLick").addEventListener("click", function(){
-        document.getElementById("8iframe").src='https://8values.github.io/quiz.html';
-        document.getElementById("8iframe").height="100%";
-        document.getElementById("8iframe").width="100%";
-        document.getElementById("8iframe").target="_parent";
+        document.getElementById("popUpDiv").style.width="600px";
+        document.getElementById("popUpDiv").style.height="900px";
+        document.getElementById("_8iframe").style.height="100%";
+        document.getElementById("_8iframe").style.width="100%";
+        document.getElementById("_8iframe").style.width="100%";
+        document.getElementById("_8iframe").src='https://8values.github.io/quiz.html';
+        document.getElementById("_8iframe").style.zoom=".71";
+
       });
       document.getElementById("spekrValuesClick").addEventListener("click", function(){
+        document.getElementById("popUpDiv").style.width="500px";
+        document.getElementById("popUpDiv").style.height="500px";
         document.getElementById("spekrIframe").src='http://spekr.org/';
-        document.getElementById("spekrIframe").height="100%";
-        document.getElementById("spekrIframe").width="100%";
+        document.getElementById("spekrIframe").style.height="100%";
+        document.getElementById("spekrIframe").style.width="100%";
         document.getElementById("spekrIframe").target="_parent";
+
       });
 });
