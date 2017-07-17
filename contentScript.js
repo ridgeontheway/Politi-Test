d2af8756e2e8af16cb1cb674d789cd38a928e8b3
George Ridgway <georger361@gmail.com>
2017-07-17 11:00:58 -0500

    Blue bar now gone from iframe when in 'full screen'
    still need to fix the size issue with the iframes

--- contentScript.js
+++ contentScript.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
       document.getElementById("8valuesCLick").addEventListener("click", function(){
-        document.getElementById("popUpDiv").style.width="600px";
-        document.getElementById("popUpDiv").style.height="900px";
-        document.getElementById("_8iframe").style.height="100%";
-        document.getElementById("_8iframe").style.width="100%";
-        document.getElementById("_8iframe").style.width="100%";
-        document.getElementById("_8iframe").src='https://8values.github.io/quiz.html';
-        document.getElementById("_8iframe").style.zoom=".71";
-
-      });
-      document.getElementById("spekrValuesClick").addEventListener("click", function(){
         document.getElementById("popUpDiv").style.width="500px";
         document.getElementById("popUpDiv").style.height="500px";
-        document.getElementById("spekrIframe").src='http://spekr.org/';
+
+        document.getElementById("_8iframe").style.height="100%";
+        document.getElementById("_8iframe").style.width="100%";
+
+        document.getElementById("_8iframe").target="_parent";
+        document.getElementById("_8iframe").src='https://8values.github.io/quiz.html';
+      });
+
+      document.getElementById("spekrValuesClick").addEventListener("click", function(){
+        document.getElementById("spekrValuesClick").style.display="none";
+        document.getElementById("8valuesCLick").style.display="none";
+
+        document.getElementById("popUpDiv").style.width="500px";
+        document.getElementById("popUpDiv").style.height="580px";
+
         document.getElementById("spekrIframe").style.height="100%";
         document.getElementById("spekrIframe").style.width="100%";
-        document.getElementById("spekrIframe").target="_parent";
+        document.getElementById("spekrIframe").style.position="absolute";
 
+        document.getElementById("spekrIframe").target="_parent";
+        document.getElementById("spekrIframe").src='http://spekr.org/';
       });
 });
