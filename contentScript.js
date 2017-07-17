b10cbc7272390f05b2d64f1084ec03df0d8acda0
George Ridgway <georger361@gmail.com>
2017-07-17 14:48:13 -0500

    Iframe content issue 'fixed'
    since I dont host the content in the iframe, I cant change the size of the text ;/, fixed the issue by expanding the div

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
       document.getElementById("8valuesCLick").addEventListener("click", function(){
-        document.getElementById("popUpDiv").style.width="500px";
-        document.getElementById("popUpDiv").style.height="500px";
+        document.getElementById("spekrValuesClick").style.display="none";
+        document.getElementById("8valuesCLick").style.display="none";
 
-        document.getElementById("_8iframe").style.height="100%";
-        document.getElementById("_8iframe").style.width="100%";
+        document.getElementById("popUpDiv").style.width="800px";
+        document.getElementById("popUpDiv").style.height="580px";
 
-        document.getElementById("_8iframe").target="_parent";
+
         document.getElementById("_8iframe").src='https://8values.github.io/quiz.html';
+        document.getElementById("_8iframe").target="_parent";
+
+        document.getElementById("_8iframe").style.width="100%";
+        document.getElementById("_8iframe").style.height="580px";
+
+        document.getElementById("_8iframe").style.WebkitTransform = 'scale(1)';
+
       });
 
       document.getElementById("spekrValuesClick").addEventListener("click", function(){
@@ -17,10 +24,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("popUpDiv").style.width="500px";
         document.getElementById("popUpDiv").style.height="580px";
 
+        document.getElementById("spekrIframe").src='http://spekr.org/';
+        document.getElementById("spekrIframe").target="_parent";
+
         document.getElementById("spekrIframe").style.height="580px";
         document.getElementById("spekrIframe").style.width="100%";
-
-        document.getElementById("spekrIframe").target="_parent";
-        document.getElementById("spekrIframe").src='http://spekr.org/';
       });
 });
