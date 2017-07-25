4d788a98065b94a96fdae4d07bd7a821a39c016b
George Ridgway <georger361@gmail.com>
2017-07-25 16:42:04 -0500

    fixed bugs when re-directing iframe

--- contentScript.js
+++ contentScript.js
@@ -19,23 +19,25 @@ document.addEventListener('DOMContentLoaded', function () {
       });
 
       document.getElementById("_8PopupImg").addEventListener("click", function(){
-        document.getElementById("spekrIframe").style.display="none";
+        document.getElementById("spekrIframe").style.visibility="hidden";
+        document.getElementById("_8iframe").style.visibility="visible";
+
         document.getElementById("popUpDiv").style.width="800px";
         document.getElementById("popUpDiv").style.height="580px";
 
         document.getElementById("_8iframe").src='https://8values.github.io/quiz.html';
         document.getElementById("_8iframe").target="_parent";
 
-        document.getElementById("_8iframe").style.visibility="visible";
         document.getElementById("_8iframe").style.width="100%";
-        document.getElementById("_8iframe").style.height="580px";
         document.getElementById("_8iframe").style.height="580px";
 
         document.getElementById("bar").style.visibility="visible";
       });
 
       document.getElementById("spekrPopupImg").addEventListener("click", function(){
-        document.getElementById("_8iframe").style.display="none";
+        document.getElementById("_8iframe").style.visibility="hidden";
+        document.getElementById("spekrIframe").style.visibility="visible";
+
         document.getElementById("popUpDiv").style.width="500px";
         document.getElementById("popUpDiv").style.height="580px";
 
